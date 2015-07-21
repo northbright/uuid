a7bd2145b0a276e14429d3ef0ac49c5cbbad84cb
Frank Xu <frank.xu@northbright.com>
2015-07-21 13:48:19 +0800

    1st commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# uuid
+
+Package uuid creates pseudo uuid by using crypto/rand.
+
+#### Documentation
+* [API Reference](http://godoc.org/github.com/northbright/uuid)
+
+#### License
+* [MIT License](./LICENSE)
---
+++ uuid_test.go
@@ -0,0 +1,14 @@
+package uuid_test
+
+import (
+	"fmt"
+	"github.com/northbright/uuid"
+)
+
+func ExampleNew() {
+	for i := 0; i < 5; i++ {
+		uuid, _ := uuid.New()
+		fmt.Printf("%v\n", uuid)
+	}
+	// Output
+}
